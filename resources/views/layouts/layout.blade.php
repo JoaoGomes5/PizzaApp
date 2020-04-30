4489ff2136c3fb84eb1ab1dcdd969f2af60d3868
Joao Gomes <joaopfg.2002@gmail.com>
2020-04-30 14:47:31 +0100

    Layout, css and image

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="/css/main.css">
+    </head>
+    <body>
+
+        @yield('content')
+
+    </body>
+
+    <footer>
+    <i class="fa fa-copyright" aria-hidden="true">Copyright {{date('Y')}} Jo's Pizzas</i>
+    </footer>
+    </html>
+
+
--- resources/views/pizzas.blade.php
+++ resources/views/pizzas.blade.php
@@ -1,69 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.layout')
 
-        <title>Laravel</title>
+    @section('content')
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
         <div class="flex-center position-ref full-height">
 
 
@@ -83,17 +21,23 @@
 
                 <div>
                   {{$loop->index}}  {{$pizza['type']}} - {{$pizza['base']}}
+
                   @if($loop->first)
+
                     <span>- first in the loop</span>
+
                   @endif
+
                   @if($loop->last)
+
                     <span>Last in loop</span>
+
                   @endif
+                  
                 </div>
 
             @endforeach
 
             </div>
         </div>
-    </body>
-</html>
+    @endsection
